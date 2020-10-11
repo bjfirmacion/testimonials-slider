329483fcc07ad12c5a03b97255fcaa23a38d3581
BJ Firmacion <bj.firmacion@gmail.com>
2020-10-11 22:53:41 +0800

    slider bug fixes

--- js/slider.js
+++ js/slider.js
@@ -17,6 +17,7 @@ by each slide's width and margin multiplied by the number of slides tracked in c
 ===================================== */
 
 // SELECTORS
+const container = document.querySelector('.slider');
 const slider = document.querySelector('.slider__slides');
 const slides = document.querySelectorAll('.slider__slide');
 const prevBtn = document.querySelector('.slider__button--prev');
@@ -24,9 +25,9 @@ const nextBtn = document.querySelector('.slider__button--next');
 const numSlides = slides.length;
 
 // DETERMINE THE LENGTH REQUIRED TO MOVE HORIZONTALLY TO THE NEXT SLIDE
-const slideWidth = slides[0].offsetWidth;
-const slideMarginRight = parseInt(getComputedStyle(slides[0]).marginRight);
-const moveX = slideWidth + slideMarginRight;
+let slideWidth = slides[0].offsetWidth;
+let slideMarginRight = parseInt(getComputedStyle(slides[0]).marginRight);
+let moveX = slideWidth + slideMarginRight;
 
 // CLONE FIRST AND LAST SLIDES AND ADD TO SLIDER
 const firstClone = slides[0].cloneNode(true);
@@ -42,60 +43,47 @@ const allSlides = document.querySelectorAll('.slider__slide'); //allSlides inclu
 
 // SHOW THE FIRST SLIDE
 slides[0].classList.add('active');
-allSlides.forEach(slide => {
-    slide.style.transform = `translateX(${-moveX}px)`;
-});
+slider.style.transform = `translateX(${-moveX}px)`;
 
 // SET COUNTER AND ADD EVENT LISTENERS TO BUTTON;
 // EACH BUTTON CLICK SHOULD MOVE ALL SLIDES HORIZONTALLY BY MOVEX AMOUNT COUNTER TIMES
 let counter = 1;
 
 nextBtn.addEventListener('click', () => {
-    allSlides[counter].classList.remove('active');
     counter++;
-    allSlides[counter].classList.add('active');
-    allSlides.forEach(slide => {
-        slide.style.transition = 'transform 0.5s ease-in-out, opacity 0.3s';
-        slide.style.transform = `translateX(${-(moveX * counter)}px)`;
-    });
+    slider.style.transition = 'transform 0.5s ease-in-out, opacity 0.2s';
+    slider.style.transform = `translateX(${-(moveX * counter)}px)`;
+    if (counter >= allSlides.length) counter = numSlides;
     console.log(counter);
 });
 
 prevBtn.addEventListener('click', () => {
-    allSlides[counter].classList.remove('active');
     counter--;
-    allSlides[counter].classList.add('active');
-    allSlides.forEach(slide => {
-        slide.style.transform = `translateX(${-(moveX * counter)}px)`;
-    });
-    console.log(counter, allSlides[counter].id);
+    slider.style.transition = 'transform 0.5s ease-in-out, opacity 0.2s';
+    slider.style.transform = `translateX(${-(moveX * counter)}px)`;
+    if (counter < 0) counter = 0
 });
 
 // CHECK FOR FIRSTCLONE AND LASTCLONE AND RESET THE SLIDES ACCORDINGLY
-//DEBUG THIS!!!
-allSlides.forEach(slide => {
-    slide.addEventListener('transitionend', () => {
-        if (allSlides[counter].id === 'first-clone') {
-            allSlides.forEach(slide => {
-                slide.style.transition = 'none';
-            });
-            counter = 1;
-            allSlides[counter].classList.add('active');
-            allSlides.forEach(slide => {
-                slide.style.transform = `translateX(${-(moveX * counter)}px)`;
-            });
-        }
+slider.addEventListener('transitionend', () => {
+    if (allSlides[counter].id === 'first-clone') {
+        slider.style.transition = 'none';
+        counter = 1;
+        slider.style.transform = `translateX(${-(moveX * counter)}px)`;
+    }
 
-        if (allSlides[counter].id === 'last-clone') {
-            allSlides.forEach(slide => {
-                slide.style.transition = 'none';
-            });
-            counter = numSlides - 1;
-            allSlides[counter].classList.add('active');
-            allSlides.forEach(slide => {
-                slide.style.transform = `translateX(${-(moveX * counter)}px)`;
-            });
-        }
+    if (allSlides[counter].id === 'last-clone') {
+        slider.style.transition = 'none';
+        counter = numSlides;
+        slider.style.transform = `translateX(${-(moveX * counter)}px)`;
+    }
+});
 
-    });
+// ADJUST THE SLIDER POSITION WHEN THE SLIDER/VIEWPORT IS RESIZED
+window.addEventListener('resize', () => {
+    slideWidth = slides[0].offsetWidth;
+    slideMarginRight = parseInt(getComputedStyle(slides[0]).marginRight);
+    moveX = slideWidth + slideMarginRight;
+    slider.style.transition = 'none';
+    slider.style.transform = `translateX(${-(moveX * counter)}px)`;
 });
